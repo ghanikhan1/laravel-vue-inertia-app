fbbbc653b4aa67376abf667f0c0c264b7b23c870
UsmanGhani <ghanikhandev@gmail.com>
2025-03-26 18:01:31 +0000

    Reusing styles component with TailwindCSS

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,8 +1,26 @@
 <template>
-    <div>
-        <Price :price="listing.price" class="text-2xl font-bold" />
-        <ListingSpace :listing="listing" class="text-lg" />
-        <ListingAddress :listing="listing" class="text-gray-500" />
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">
+                No Images
+            </div>
+        </Box>
+        <div class="md:col-span-5 flex flex-col gap-5">
+            <Box>
+                <template #header>
+                    Basic Info
+                </template>
+                <Price :price="listing.price" class="text-2xl font-bold"/>
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray-500"/>
+            </Box>
+            <Box>
+                <template #header>
+                    Offer
+                </template>
+                Make an offer
+            </Box>
+        </div>
     </div>
 </template>
 
@@ -10,6 +28,7 @@
 import ListingAddress from "@/components/ListingAddress.vue";
 import Price from "../../components/Price.vue";
 import ListingSpace from "../../components/ListingSpace.vue";
+import Box from "../../UI/Box.vue";
 
 defineProps({
     listing: Object,
