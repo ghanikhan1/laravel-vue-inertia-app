4e75b18467b0b6061c46b4cca43abd9ccb1113ae
UsmanGhani <ghanikhandev@gmail.com>
2025-04-02 16:22:29 +0100

    Created Filters on Listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -28,12 +28,94 @@ class ListingController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filters = $request->only([
+            'priceFrom',
+            'priceTo',
+            'beds',
+            'baths',
+            'areaFrom',
+            'areaTo',
+        ]);
+
+        $query = Listing::orderByDesc('created_at')->when(
+            $filters['priceFrom'] ?? false,
+        fn($query, $value) => $query->where('price', '>=', $value)
+        )->when(
+            $filters['priceTo'] ?? false,
+            fn($query, $value) => $query->where('price', '<=', $value)
+        )->when(
+            $filters['beds'] ?? false,
+            fn($query, $value) => $query->where('beds', (int)$value < 5 ? '=' : '>=' , $value)
+        )->when(
+            $filters['baths'] ?? false,
+            fn($query, $value) => $query->where('baths', (int)$value < 5 ? '=' : '>=' ,$value)
+        )->when(
+            $filters['areaFrom'] ?? false,
+            fn($query, $value) => $query->where('area', '>=', $value)
+        )->when(
+            $filters['areaTo'] ?? false,
+            fn($query, $value) => $query->where('area', '<=', $value)
+        )->when(
+            $filters['areaTo'] ?? false,
+            fn($query, $value) => $query->where('area', '<=', $value)
+        );
+
+//        if ($filters['priceFrom'] ?? false) {
+//            $query->where('price', '>=', $filters['priceFrom']);
+//        }
+
+//        if ($filters['priceTo'] ?? false) {
+//            $query->where('price', '<=', $filters['priceTo']);
+//        }
+
+//        if ($filters['beds'] ?? false) {
+//            $query->where('beds', $filters['beds']);
+//        }
+
+//        if ($filters['baths'] ?? false) {
+//            $query->where('baths', $filters['baths']);
+//        }
+
+//        if ($filters['areaFrom'] ?? false) {
+//            $query->where('area', '>=', $filters['areaFrom']);
+//        }
+
+//        if ($filters['areaTo'] ?? false) {
+//            $query->where('area', '<=', $filters['areaTo']);
+//        }
+
         return Inertia(
             'Listing/Index',
             [
-                'listings' => Listing::all()
+                'filters' => $filters,
+
+//                'listings' => $query->paginate(10)->withQueryString()
+
+                'listings' => Listing::orderByDesc('created_at')->when(
+                    $filters['priceFrom'] ?? false,
+                    fn($query, $value) => $query->where('price', '>=', $value)
+                )->when(
+                    $filters['priceTo'] ?? false,
+                    fn($query, $value) => $query->where('price', '<=', $value)
+                )->when(
+                    $filters['beds'] ?? false,
+                    fn($query, $value) => $query->where('beds', (int)$value < 5 ? '=' : '>=' , $value)
+                )->when(
+                    $filters['baths'] ?? false,
+                    fn($query, $value) => $query->where('baths', (int)$value < 5 ? '=' : '>=' ,$value)
+                )->when(
+                    $filters['areaFrom'] ?? false,
+                    fn($query, $value) => $query->where('area', '>=', $value)
+                )->when(
+                    $filters['areaTo'] ?? false,
+                    fn($query, $value) => $query->where('area', '<=', $value)
+                )->when(
+                    $filters['areaTo'] ?? false,
+                    fn($query, $value) => $query->where('area', '<=', $value)
+                )->paginate(10)->withQueryString()
+
             ]
         );
     }
